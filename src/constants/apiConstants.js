b187279471c21fd8fd6434dbc72046d327719d1c
Codrello <78147005+Codrello@users.noreply.github.com>
2021-11-14 12:56:22 +0500

    aassa

--- src/constants/apiConstants.js
+++
@@ -1,6 +0,0 @@
-let apiUrl = "https://express-admins.herokuapp.com";
-
-export const registration = `${apiUrl}/Reg`;
-export const login = `${apiUrl}/Log`;
-export const logout = `${apiUrl}/logout`;
-export const loadUser = `${apiUrl}/list`;
